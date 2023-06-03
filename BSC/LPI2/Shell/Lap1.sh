86251f9c98bd95768a1511b43a8e4c8d7609a8a6
nguyenthanhqt <nguyenthanhqt@gmail.com>
2023-06-03 08:40:16 +0700

    commit L:PI

--- BSC/LPI2/Shell/Lap1.sh
+++ BSC/LPI2/Shell/Lap1.sh
@@ -1,17 +1,19 @@
+#!/bin/bash
 echo "Nhap vao o cung muon kiem tra: "
 read hdd
 echo "=================================THONG TIN DIA CUNG $hdd ========================================="
 echo
-echo "Kiem tra ngay  thang   nam"
-hddinfo= `fdisk -l | grep $hdd`
-patitions= `fdisk -l | grep $hdd | tail -n +2 | awk '{print $1}'`
+DateTime=$(date "ngay "%d" thang "%m---%Y)
+echo "Kiem tra : $DateTime"
+hddinfo=$(fdisk -l | grep /dev/sda | head -n 1 | awk -F: '{print $2}')
+patitions=$(fdisk -l | grep $hdd | tail -n +2 | awk '{print $1}')
 echo "O cung $hdd co dung luong: $hddinfo ."
-echo "Cac phan vung o cung: $pations " 
+echo "Cac phan vung o cung \n: $patitions " 
 
-pationsInfo= `fdisk -l | grep $hdd | tail -n +2 `
+pationsInfo=$(fdisk -l | grep $hdd | tail -n +2)
 for i in $pationsInfo
     do 
-        pname=`awk 'print $1' $i`
+        pname=$(awk 'print $1' $i)
         pfortmat=`awk 'print $6' $i`
         echo "phan vung $pname co dinh dang la:  $pfortmat"
     done
